6992518184312aaf5fec9182c376699ecadb5056
Vladislav Fitc <vladislavfitz@me.com>
2013-11-29 12:52:09 +0100

    Fusionne des versions des algos avec des params differents

--- algo_glouton.py
+++ algo_glouton.py
@@ -3,12 +3,25 @@ import sys
 sys.setrecursionlimit(50000) #Augmentation du nb max de mots à traiter
 from fonctions_diverses import formate, args_corrects, enregistre, list_to_text, decoupage, measureOfTextEquilibrium, possibleString, longueur, blancLigne
 
+def choix(texte, n, m):
+    
+    if m == 'b':
+        return equilibreBl(texte,n)
+    elif m == 'e':
+        return equilibreEtendue(texte,n)
+
 #---------------------------------------------------------------------------------
 #------------------------------Approche gloutonne---------------------------------
 #---------------------------------------------------------------------------------
 
+###########################################################################################################
+############################### Algo avec blancLigne comme parametre ######################################
+###########################################################################################################
 
-def equilibreGlouton(words, textEquilibre, n):
+
+
+
+def equilibreGloutonBl(words, textEquilibre, n):
     if(words == []):
         return textEquilibre
     else:
@@ -34,14 +47,56 @@ def equilibreGlouton(words, textEquilibre, n):
             return equilibreGlouton(words, textEquilibre, n)
         
 
-def equilibre(texte, n):
+def equilibreBl(texte, n):
     textEq = [[texte[0][:]]]
     texte.pop(0)
-    result = equilibreGlouton(texte, textEq, n)
+    result = equilibreGloutonBl(texte, textEq, n)
     #print result
     return result, measureOfTextEquilibrium(result, n)
 
+###########################################################################################################
+############################### Algo avec etendu comme parametre ##########################################
+###########################################################################################################
 
+def equilibreGloutonEtendu(words, textEquilibre, n):
+    if(words == []):
+        return textEquilibre
+    else:
+        if(textEquilibre == []):
+            textEquilibre = [[words[0][:]]]
+            words.pop(0)
+            return equilibreGlouton(words, textEquilibre, n)
+        
+        textWithTransfer = textEquilibre[:]
+        textWithTransfer.append([words[0][:]])
+        print "WT", textWithTransfer
+        
+        if(possibleString(textEquilibre[-1] + [words[0]], n)):
+            lastString = textEquilibre[-1][:] + [words[0][:]]
+            textWoTransfer = textEquilibre[:]
+            textWoTransfer.pop(-1)
+            textWoTransfer.append(lastString)
+            words.pop(0)
+            print "WoT", textWoTransfer
+            
+            maxMinWoT = longShortDistance(textWoTransfer)
+            distanceWoT = maxMinWoT[0]-maxMinWoT[1]
+            maxMinWT = longShortDistance(textWithTransfer)
+            distanceWT = maxMinWT[0]-maxMinWT[1]
+            if(distanceWoT < distanceWT):
+                return equilibreGlouton(words, textWoTransfer, n)
+            else:
+                return equilibreGlouton(words, textWithTransfer, n)
+        else:
+            words.pop(0)
+            return equilibreGlouton(words, textWithTransfer, n)
+
+def equilibreEtendu(texte, n):
+    textEq = [[texte[0][:]]]
+    texte.pop(0)
+    result = equilibreGloutonEtendu(texte, textEq, n)
+    #print result
+    return result, measureOfTextEquilibrium(result, n)
 
 
 #------------------------------------------Main--------------------------------------
@@ -51,7 +106,7 @@ def equilibre(texte, n):
 #On récupère le texte à découper dans un fichier, et on écrit le texte découpé dans un autre
 
 def main():
-    f=equilibre
+    f=choix
     decoupage(f)
 
 
